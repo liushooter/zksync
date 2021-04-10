074b58753ba4231830a4eed1501f1d8e7ef79443
liushooter <shooter.hero@gmail.com>
2021-04-10 15:59:41 +0800

    06

--- node/server.js
+++ node/server.js
@@ -1,36 +1,31 @@
 const ethers = require("ethers")
 const zksync = require("zksync")
 
+const _privKey = ""
+
 function getWallet() {
   const wallet = new ethers.Wallet.createRandom()
 
-  const privateKey = wallet.privateKey
-  const publicKey = wallet.publicKey
-  const addr = wallet.address
+  console.log("privateKey: ", wallet.privateKey)
+  console.log("publicKey: ", wallet.publicKey)
+  console.log("addr: ", wallet.address)
 
-  console.log("privateKey: ", privateKey)
-  console.log("publicKey: ", publicKey)
-  console.log("addr: ", addr)
+  return wallet
 }
 
-async function getBalanceAndSendTx() {
-
-  const _privKey = ""
-
-  const toAddr = ""
+async function getZkWallet(privKey) {
   const ethersProvider = ethers.getDefaultProvider("ropsten")
 
-  // console.log(ethersProvider)
-
   // Create ethereum wallet using ethers.js
-  const ethWallet = new ethers.Wallet(_privKey).connect(ethersProvider)
+  const ethWallet = new ethers.Wallet(privKey).connect(ethersProvider)
 
   const zkProvider = await zksync.getDefaultProvider("ropsten")
   // Derive zksync.Signer from ethereum wallet.
   const zkWallet = await zksync.Wallet.fromEthSigner(ethWallet, zkProvider)
+  return zkWallet
+}
 
-  /// Checking zkSync account balance
-
+async function getBalance(zkWallet) {
   // Committed state is not final yet
   let committedETHBalance = await zkWallet.getBalance("ETH")
 
@@ -40,29 +35,28 @@ async function getBalanceAndSendTx() {
   console.log("committedETHBalance: ", committedETHBalance)
   console.log("verifiedETHBalance: ", verifiedETHBalance)
 
-  /// AccountState
-  const state = await zkWallet.getAccountState()
+  const accountId = await zkWallet.getAccountId()
 
-  const committedBalances = state.committed.balances
-  committedETHBalance = committedBalances["ETH"]
+  if (accountId == undefined) {
+    // throw new Error("Unknown Account Id")
+    console.log("Unknown Account Id")
+  } else {
+    console.log("accountId: ", accountId)
 
-  const ethBalance = ethers.utils.formatEther(committedETHBalance)
+    let isSigningKeySet = await zkWallet.isSigningKeySet()
+    console.log("isSigningKeySet: ", isSigningKeySet)
 
-  console.log("ethBalance: ", ethBalance, "ETH")
+    /// AccountState
+    const state = await zkWallet.getAccountState()
+    const committedBalances = state.committed.balances
 
-  let isSigningKeySet = await zkWallet.isSigningKeySet()
-  let accountId = await zkWallet.getAccountId()
-
-  console.log("isSigningKeySet: ", isSigningKeySet)
-  console.log("accountId: ", accountId)
-
-  // L1 to L2
-  // depositETHToZksync(zkWallet, zkWallet.address())
-
-  sendTx(zkWallet, toAddr) // L2 to L2
+    committedETHBalance = committedBalances["ETH"]
+    const ethBalance = ethers.utils.formatEther(committedETHBalance)
+    console.log("ethBalance: ", ethBalance, "ETH")
+  }
 }
 
-async function depositETHToZksync(zkWallet, newAddr){ // from eth to zksync
+async function depositETHToZksync(zkWallet, newAddr){ // from eth to zksync , L1 to L2
   const deposit = await zkWallet.depositToSyncFromEthereum({
     depositTo: newAddr, // 可以给自己的地址转账，也可以是别人的
     token: "ETH",
@@ -70,58 +64,58 @@ async function depositETHToZksync(zkWallet, newAddr){ // from eth to zksync
   });
 
   const depositReceipt = await deposit.awaitReceipt();
-
   const depositVerifyReceipt = await deposit.awaitVerifyReceipt();
 
   console.log("depositReceipt: ", depositReceipt);
   console.log("depositVerifyReceipt: ", depositVerifyReceipt);
 }
 
-async function sendTx(zkWallet, newAddr) { // L2 to L2
+async function sendTx(zkWallet, newAddr, cost) { // L2 to L2
+    const isSigningKeySet = await zkWallet.isSigningKeySet()
 
-  const isSigningKeySet = await zkWallet.isSigningKeySet()
+    // L2 转账前，必须先解锁
+    if (!isSigningKeySet) {
+      if ((await zkWallet.getAccountId()) == undefined) {
+        throw new Error("Unknown Account Id")
+      }
 
-  // L2 转账前，必须先解锁
-  if (!isSigningKeySet) {
-    if ((await zkWallet.getAccountId()) == undefined) {
-      throw new Error("Unknown account");
+      // As any other kind of transaction, `ChangePubKey` transaction requires fee.
+      // User doesn't have (but can) to specify the fee amount. If omitted, library will query zkSync node for
+      // the lowest possible amount.
+      const changePubkey = await zkWallet.setSigningKey({
+        ethAuthType: "ECDSA", // 显式指定验证类型
+        feeToken: "ETH",
+        // fee: ethers.utils.parseEther("0.001")
+      });
+
+      // Wait until the tx is committed
+      const receipt = await changePubkey.awaitReceipt();
+
+      console.log("changePubkey: ", changePubkey)
+      console.log()
+      console.log("receipt: ", receipt)
     }
 
-    // As any other kind of transaction, `ChangePubKey` transaction requires fee.
-    // User doesn't have (but can) to specify the fee amount. If omitted, library will query zkSync node for
-    // the lowest possible amount.
-    const changePubkey = await zkWallet.setSigningKey({
-      ethAuthType: "ECDSA", // 显式指定验证类型
-      feeToken: "ETH",
-      fee: ethers.utils.parseEther("0.001")
-    });
+    const amount = zksync.utils.closestPackableTransactionAmount(
+      ethers.utils.parseEther(cost))
 
-    // Wait until the tx is committed
-    const receipt = await changePubkey.awaitReceipt();
+    const fee = zksync.utils.closestPackableTransactionFee(
+      ethers.utils.parseEther("0.001"))
 
-    console.log("changePubkey: ", changePubkey)
-    console.log()
-    console.log("receipt: ", receipt)
+    const transfer = await zkWallet.syncTransfer({
+      to: newAddr,
+      token: "ETH",
+      amount,
+      fee,
+    })
+
+    console.log("transfer: ", transfer)
+
+    return transfer
   }
 
-  const amount = zksync.utils.closestPackableTransactionAmount(
-    ethers.utils.parseEther("0.111"))
-
-  const fee = zksync.utils.closestPackableTransactionFee(
-    ethers.utils.parseEther("0.001"))
-
-  const transfer = await zkWallet.syncTransfer({
-    to: newAddr,
-    token: "ETH",
-    amount,
-    fee,
-  })
-
-
-  console.log("transfer: ", transfer);
-}
-
-function main() {
+///
+async function main() {
   const args = process.argv
 
   let num = 10
@@ -132,13 +126,20 @@ function main() {
 
   }
 
-  for (var i = 0; i < num; i++) {
-    getWallet()
+  let wallets = []
+  for (let i = 0; i < num; i++) {
+    wallets.push(getWallet())
     console.log("----------")
   }
+
+  const zkWalletObj = await getZkWallet(_privKey)
+
+  for (const wallet of wallets) {
+    const addr = wallet.address
+    console.log(addr);
+  }
+
 
 }
 
 main()
-
-// getBalanceAndSendTx()
